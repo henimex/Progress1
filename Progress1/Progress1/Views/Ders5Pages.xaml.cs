c1c002dcdb2b0d3223732008b528eecf528adc7e
henimex <49072861+henimex@users.noreply.github.com>
2021-01-16 15:55:33 +0300

    Add project files.

---
+++ Progress1/Progress1/Views/Ders5Pages.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Progress1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ders5Pages : ContentPage
+    {
+        public Ders5Pages()
+        {
+            InitializeComponent();
+        }
+    }
+}
